593976e60a58ce90e6e7212092972a8ed4898212
Nitrogen-Dioxide <nitrogen.dioxide99@gmail.com>
2015-09-02 11:15:34 -0300

    Use list instead of arrays on UTS
    Better, because list doesn't have fixed sizes.

--- UnseenTube/src/net/nitro/UnseenTubeStack.java
+++ UnseenTube/src/net/nitro/UnseenTubeStack.java
@@ -1,15 +1,17 @@
 package net.nitro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnseenTubeStack extends Messages {
 	final boolean DEBUG;
 	final int MIN_VIEW;
 	final String LINK;
 	int page = 2;
-	int set = 0;
-	public int last;
-	public String[] titles = new String[200];
-	public String[] links = new String[200];
-	public int[] views = new int[200];
+	public int last = 0;
+	public List<Integer> views = new ArrayList<Integer>();
+	public List<String> titles = new ArrayList<String>();
+	public List<String> links = new ArrayList<String>();
 
 	UnseenTubeStack(String search, boolean doDebug, int minView) {
 		LINK = "https://www.youtube.com/results?filters=video%2C+year&search_query="
@@ -33,20 +35,14 @@ public class UnseenTubeStack extends Messages {
 			cvt.getMin();
 			do {
 				if (MIN_VIEW >= cvt.min && get == true) {
-					titles[set] = scr.titles[cvt.minIndex];
-					views[set] = cvt.views[cvt.minIndex];
-					links[set] = scr.links[cvt.minIndex];
+					titles.add(scr.titles[cvt.minIndex]);
+					views.add(cvt.views[cvt.minIndex]);
+					links.add(scr.links[cvt.minIndex]);
 					if (DEBUG)
 						notify("Succesfully added.");
-					set++;
-					if (set > ((titles.length + links.length + views.length) / 3) - 1) {
-						if (DEBUG)
-							notify("Too many videos!");
-						break;
-					}
-					;
 					cvt.views[cvt.minIndex] = MIN_VIEW * 2 + 1;
 					cvt.getMin();
+					last++;
 				}
 
 				if (MIN_VIEW < cvt.min) {
@@ -63,7 +59,6 @@ public class UnseenTubeStack extends Messages {
 						notify("Looped");
 				}
 			} while (get == true);
-			last = set;
 			notify("Terminated succesfully");
 			return true;
 		} catch (Exception e) {
